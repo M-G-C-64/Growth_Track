03e67440eed45aef011037a2922b84e7bfff5856
M-G-C-64 <18131a04c1@gvpce.ac.in>
2022-08-17 20:08:55 +0530

    added nf

--- visuv.py
+++ visuv.py
@@ -5,39 +5,48 @@ import matplotlib.pyplot as plt
 from scipy.interpolate import make_interp_spline
 
 
-def plot(path, type='bar', sheet_name=None):
+def plot(path,smoothness=True, type='line', sheet_name=None):
     if sheet_name:
         var = pd.read_excel(path, sheet_name=sheet_name)
     else:
         var = pd.read_excel(path)
-
     lst = list(var.columns.values)
-    for i in (lst[1:]):
-        #print(var['Date'][0], var['Date'][var.index[-1]])
-        index = pd.date_range(var['Date'][0], var['Date'][var.index[-1]])
-        df = pd.DataFrame(list(var[i]), index=index, columns=["values"])
-        fig, axe = plt.subplots(1,1, figsize=(10,5))
-        x = df.index
-        y = df.values
-        X_new = np.linspace(0, len(x), 300)
-        spline1 = make_interp_spline(
-            [j for j in range(0, len(x))],
-            y,
-            k=3
-        )
 
-        y_new = spline1(X_new)
+    if smoothness and type == 'line':
+        for i in (lst[1:]):
+            #print(var['Date'][0], var['Date'][var.index[-1]])
+            index = pd.date_range(var['Date'][0], var['Date'][var.index[-1]])
+            df = pd.DataFrame(list(var[i]), index=index, columns=["values"])
+            fig, axe = plt.subplots(1,1, figsize=(10,5))
+            x = df.index
+            y = df.values
+            X_new = np.linspace(0, len(x), 300)
+            spline1 = make_interp_spline(
+                [j for j in range(0, len(x))],
+                y,
+                k=3
+            )
 
-        plt.plot(X_new, y_new, color='red')
+            y_new = spline1(X_new)
 
-        #plt.plot(var['Date'], var[i])
-        plt.title(path)
-        plt.legend(lst[1:])
-        
-        x = list(x.astype(str))
-        plt.plot(x, y, linestyle=" ", alpha=0.75)
-        xt = [x[i] for i in range(0,len(x),5)]
-        plt.xticks(xt,rotation="vertical")
+            plt.plot(X_new, y_new, color='red')
+
+            #plt.plot(var['Date'], var[i])
+            plt.title(path)
+            plt.ylabel(i)
+            
+            x = list(x.astype(str))
+            plt.plot(x, y, linestyle=" ", alpha=0.75)
+            xt = [x[i] for i in range(0,len(x),5)]
+            plt.xticks(xt,rotation="vertical")
+
+    else:
+        for i in lst[1:]:
+            var.plot(kind=type, x='Date', y=i)
+            plt.title(path)
+            plt.ylabel(i)
+            plt.legend(lst[1:])
+ 
         
     plt.show()
 
@@ -45,8 +54,9 @@ def plot(path, type='bar', sheet_name=None):
 
 
 
-plot('growth_log/career/problem_solving.xlsx', 'line')
+plot('growth_log/career/problem_solving.xlsx')
+plot('growth_log/health/nofap.xlsx', False, 'bar')
 plot('growth_log/health/growth.xlsx')
-plot('growth_log/personality/self_help.xlsx', 'line')
+plot('growth_log/personality/self_help.xlsx', False, 'bar')
 
 
