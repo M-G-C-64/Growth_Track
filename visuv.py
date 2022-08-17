546cf59e2a8ac19ad8f5ccb67cfa3c235f8f2387
M-G-C-64 <18131a04c1@gvpce.ac.in>
2022-08-17 23:32:45 +0530

    try except

--- visuv.py
+++ visuv.py
@@ -14,38 +14,45 @@ def plot(path,smoothness=True, type='line', sheet_name=None):
 
     if smoothness and type == 'line':
         for i in (lst[1:]):
-            #print(var['Date'][0], var['Date'][var.index[-1]])
-            index = pd.date_range(var['Date'][0], var['Date'][var.index[-1]])
-            df = pd.DataFrame(list(var[i]), index=index, columns=["values"])
-            fig, axe = plt.subplots(1,1, figsize=(10,5))
-            x = df.index
-            y = df.values
-            X_new = np.linspace(0, len(x), 300)
-            spline1 = make_interp_spline(
-                [j for j in range(0, len(x))],
-                y,
-                k=3
-            )
+            try:
+                #print(var['Date'][0], var['Date'][var.index[-1]])
+                index = pd.date_range(var['Date'][0], var['Date'][var.index[-1]])
+                df = pd.DataFrame(list(var[i]), index=index, columns=["values"])
+                fig, axe = plt.subplots(1,1, figsize=(10,5))
+                x = df.index
+                y = df.values
+                X_new = np.linspace(0, len(x), 300)
+                spline1 = make_interp_spline(
+                    [j for j in range(0, len(x))],
+                    y,
+                    k=3
+                )
 
-            y_new = spline1(X_new)
+                y_new = spline1(X_new)
 
-            plt.plot(X_new, y_new, color='red')
+                plt.plot(X_new, y_new, color='red')
 
-            #plt.plot(var['Date'], var[i])
-            plt.title(path)
-            plt.ylabel(i)
-            
-            x = list(x.astype(str))
-            plt.plot(x, y, linestyle=" ", alpha=0.75)
-            xt = [x[i] for i in range(0,len(x),5)]
-            plt.xticks(xt,rotation="vertical")
+                #plt.plot(var['Date'], var[i])
+                plt.title(path)
+                plt.ylabel(i)
+                
+                x = list(x.astype(str))
+                plt.plot(x, y, linestyle=" ", alpha=0.75)
+                xt = [x[i] for i in range(0,len(x),5)]
+                plt.xticks(xt,rotation="vertical")
+            except:
+                print("exception occured")
 
     else:
+
         for i in lst[1:]:
-            var.plot(kind=type, x='Date', y=i)
-            plt.title(path)
-            plt.ylabel(i)
-            plt.legend(lst[1:])
+            try:
+                var.plot(kind=type, x='Date', y=i)
+                plt.title(path)
+                plt.ylabel(i)
+                plt.legend(lst[1:])
+            except:
+                print("exception occured")
  
         
     plt.show()
@@ -58,5 +65,6 @@ plot('growth_log/career/problem_solving.xlsx', False, 'bar')
 plot('growth_log/health/nofap.xlsx')
 plot('growth_log/health/growth.xlsx')
 plot('growth_log/personality/self_help.xlsx')
+plot('growth_log/misc/satisfaction.xlsx', False, 'bar')
 
 
